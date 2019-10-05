8b9b81087cffc0df1c0d6df3d7d09c31fa1457e7
XanderJames1910 <xanderjames1910@gmail.com>
2019-10-05 17:29:43 -0500

    Last update

--- src/components/addReminder.js
+++ src/components/addReminder.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
 
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 class addReminder extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Ver día y recordatorios</h1>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-6'>
+            <Form onSubmit={this.handleSubmit} noValidate>
+              <Card className='shadow-sm'>
+                <Card.Header as='h5' className='text-center'>
+                  Crear un recordatorio
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title className='text-center'>Fecha seleccionada</Card.Title>
+                  <Form.Group controlId='formGroupNombre'>
+                    <Form.Label>Nombre del recordatorio</Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='nombreRecordatorio'
+                      placeholder='Escriba el nombre del recordatorio'
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId='formGroupCudad'>
+                    <Form.Label>Ciudad del recordatorio</Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='ciudad'
+                      placeholder='Ingrese la ciudad del recordatorio'
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId='formGroupProjectContent'>
+                    <Form.Label>Descripción del recordatorio</Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='content'
+                      placeholder='Explique brevemente el recordatorio'
+                      as='textarea'
+                      rows='3'
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant='primary' type='submit' block className='shadow-sm'>
+                    Guardar Recordatorio
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </Form>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/viewDay.js
+++ src/components/viewDay.js
@@ -1,54 +1,34 @@
 import React, { Component } from 'react';
 
-import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 class viewDay extends Component {
+  editReminder = e => {
+    this.props.history.push('/add-reminder/');
+  };
+
   render() {
     return (
       <div className='container'>
         <div className='row justify-content-center'>
-          <div className='col-md-4'>
-            <Form onSubmit={this.handleSubmit} noValidate>
-              <Card className='shadow-sm'>
-                <Card.Header as='h5' className='text-center'>
-                  Crear un recordatorio
-                </Card.Header>
-                <Card.Body>
-                  <Card.Title>Fecha seleccionada</Card.Title>
-                  <Form.Group controlId='formGroupNombre'>
-                    <Form.Label>Nombre del recordatorio</Form.Label>
-                    <Form.Control type='text' name='email' placeholder='Escriba el nombre del recordatorio' onChange={this.handleChange} />
-                  </Form.Group>
-                  <Form.Group controlId='formGroupCudad'>
-                    <Form.Label>Ciudad del recordatorio</Form.Label>
-                    <Form.Control
-                      type='text'
-                      name='ciudad'
-                      placeholder='Ingrese la ciudad del recordatorio'
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId='formGroupProjectContent'>
-                    <Form.Label>Descripción del proyecto</Form.Label>
-                    <Form.Control
-                      type='text'
-                      name='content'
-                      placeholder='Explique brevemente el proyecto'
-                      as='textarea'
-                      rows='3'
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </Card.Body>
-                <Card.Footer>
-                  <Button variant='primary' type='submit' block className='shadow-sm'>
-                    Guardar Recordatorio
-                  </Button>
-                </Card.Footer>
-              </Card>
-            </Form>
+          <div className='col-md-6'>
+            <Card>
+              <Card.Header as='h5'>Nombre del Recordatorio</Card.Header>
+              <Card.Body>
+                <Card.Text>Cumpleaños Madre</Card.Text>
+                <Card.Text>Quito</Card.Text>
+                <Card.Text>Recordarme comprar las flores y el pastel para el Cumpleaños de mi madre.</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant='primary' type='submit' className='shadow-sm mr-3'>
+                  Regresar al Calendario
+                </Button>
+                <Button variant='primary' onClick={this.editReminder} className='shadow-sm ml-3'>
+                  Editar día
+                </Button>
+              </Card.Footer>
+            </Card>
           </div>
         </div>
       </div>
